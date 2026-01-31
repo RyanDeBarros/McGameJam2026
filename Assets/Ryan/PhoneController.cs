9c780d6264fead3926934856db37ed06091d90ff
RyanDeBarros <ryan2004db@gmail.com>
2026-01-30 23:36:58 -0500

    Phone toggling animation

--- Assets/Ryan/PhoneController.cs
+++ Assets/Ryan/PhoneController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
@@ -5,12 +6,25 @@ using UnityEngine.InputSystem;
 public class PhoneController : MonoBehaviour
 {
     private bool open = false;
+    private float openedFactor = 0f;
+
+    [Header("Phone Animation")]
+    [SerializeField] private RectTransform visual;
+    [SerializeField] private RectTransform openedVisual;
+    [SerializeField] private RectTransform closedVisual;
+    [SerializeField] private float toggleAnimationSpeed = 5f;
+    private Coroutine visualAnimation = null;
 
     private PlayerInput playerInput;
     private InputAction toggleAction;
 
     private void Awake()
     {
+        Assert.IsNotNull(visual);
+        Assert.IsNotNull(openedVisual);
+        Assert.IsNotNull(closedVisual);
+        SetPhoneTransform();
+
         playerInput = GetComponent<PlayerInput>();
         Assert.IsNotNull(playerInput);
         toggleAction = playerInput.actions["TogglePhone"];
@@ -45,17 +59,50 @@ public class PhoneController : MonoBehaviour
     {
         if (open)
             return;
-
         open = true;
-        Debug.Log("Open Phone!");
+
+        if (visualAnimation != null)
+            StopCoroutine(visualAnimation);
+        visualAnimation = StartCoroutine(OpenPhoneAnimation());
+    }
+
+    private IEnumerator OpenPhoneAnimation()
+    {
+        while (openedFactor < 1f)
+        {
+            openedFactor += toggleAnimationSpeed * Time.deltaTime;
+            openedFactor = Mathf.Clamp01(openedFactor);
+            SetPhoneTransform();
+            yield return null;
+        }
     }
 
     public void ClosePhone()
     {
         if (!open)
             return;
-
         open = false;
-        Debug.Log("Close Phone!");
+
+        if (visualAnimation != null)
+            StopCoroutine(visualAnimation);
+        visualAnimation = StartCoroutine(ClosePhoneAnimation());
+    }
+
+    private IEnumerator ClosePhoneAnimation()
+    {
+        while (openedFactor > 0f)
+        {
+            openedFactor -= toggleAnimationSpeed * Time.deltaTime;
+            openedFactor = Mathf.Clamp01(openedFactor);
+            SetPhoneTransform();
+            yield return null;
+        }
+    }
+
+    private void SetPhoneTransform()
+    {
+        visual.transform.localScale = Vector3.Lerp(closedVisual.localScale, openedVisual.localScale, openedFactor);
+        visual.transform.position = Vector3.Lerp(closedVisual.position, openedVisual.position, openedFactor);
+        visual.transform.rotation = Quaternion.Slerp(closedVisual.rotation, openedVisual.rotation, openedFactor);
     }
 }
