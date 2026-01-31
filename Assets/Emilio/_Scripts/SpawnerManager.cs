b25b1ef61d50da524a801941cbae60227418a3b0
Emilio Chavero <116393972+Chavero2001@users.noreply.github.com>
2026-01-31 13:26:31 -0500

    Collector

--- Assets/Emilio/_Scripts/SpawnerManager.cs
+++ Assets/Emilio/_Scripts/SpawnerManager.cs
@@ -1,33 +1,39 @@
 using UnityEngine;
+using System.Collections.Generic;
+
+//Pls attach this script to any manager, and drag the baby prefab to the SerializeField
 
 public class SpawnerManager : MonoBehaviour
 {
-    private GameObject[] SpawnersDetected;
+    //private GameObject[] SpawnersDetected;
+    private List<GameObject> SpawnersDetected;
     //private GameObject[] Spawners;
     [SerializeField] GameObject Baby;
     
     private bool RandomValue; 
     private string Tag = "Spawner";
-    private Transform SpawnTransform;
 
     public int Amount = 1;
 
-
     private void Awake()
     {
-        //
-        SpawnersDetected = GameObject.FindGameObjectsWithTag(Tag);
-        Debug.Log("Found " + SpawnersDetected.Length);
+        //SpawnersDetected = GameObject.FindGameObjectsWithTag(Tag);
+        SpawnersDetected  = new List<GameObject>( GameObject.FindGameObjectsWithTag(Tag));
+        //Debug.Log("Found " + SpawnersDetected.Length);
 
         //Random value between 0 and the number of Spawners found 
         //RandomValue = Random.Range(0, SpawnersDetected.Length);
 
+        Amount = Mathf.Min(Amount, SpawnersDetected.Count);
+
         for (int i = 0; i < Amount; i++)
         {
-            int randomIndex = Random.Range(0, SpawnersDetected.Length);
+            int randomIndex = Random.Range(0, SpawnersDetected.Count);
             Transform spawnPoint = SpawnersDetected[randomIndex].transform;
 
             Instantiate(Baby, spawnPoint.position, spawnPoint.rotation);
+            Destroy(SpawnersDetected[randomIndex].gameObject);
+            SpawnersDetected.RemoveAt(randomIndex);
 
             Debug.Log("Spawned Baby at spawner " + randomIndex);
         }
