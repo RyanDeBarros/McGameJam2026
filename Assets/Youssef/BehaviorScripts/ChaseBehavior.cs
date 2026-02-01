7e087629325630150226b0d1021e1184c51562b2
youssefjango <benmounyyoussef@gmail.com>
2026-01-31 21:24:35 -0500

    fixed forgotten tag

--- Assets/Youssef/BehaviorScripts/ChaseBehavior.cs
+++ Assets/Youssef/BehaviorScripts/ChaseBehavior.cs
@@ -19,7 +19,7 @@ public class ChaseBehavior : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        player = GameObject.FindGameObjectWithTag("MC");
+        player = GameObject.FindGameObjectWithTag("Player");
         agent = animator.GetComponent<NavMeshAgent>();
         agent.speed = chasingSpeed;
     }
