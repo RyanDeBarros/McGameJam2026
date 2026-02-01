29b739d27bfefe644102a884f0aaa6df94da2f93
MauriceELkouby <151778578+MauriceELkouby@users.noreply.github.com>
2026-02-01 06:19:24 -0500

    Kiss animation
    Co-Authored-By: Youssef Ben Mouny <76130774+youssefjango@users.noreply.github.com>

--- Assets/Youssef/BehaviorScripts/JumpScareBehavior.cs
+++ Assets/Youssef/BehaviorScripts/JumpScareBehavior.cs
@@ -13,7 +13,8 @@ public class JumpScareBehavior : StateMachineBehaviour
     private GameObject player;
     private NavMeshAgent agent;
     private float time;
-    
+    private GameObject camera;
+    private GameObject arms;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -31,6 +32,14 @@ public class JumpScareBehavior : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        arms = GameObject.FindGameObjectWithTag("Arms");
+        camera.transform.localPosition = new Vector3(0, 0, 1.31f);
+        arms.transform.localPosition = new Vector3(
+            arms.transform.localPosition.x,
+            arms.transform.localPosition.y,
+            -1.5f
+        );
         time += Time.deltaTime;
         player.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         player = GameObject.FindGameObjectWithTag("Player");
@@ -40,6 +49,12 @@ public class JumpScareBehavior : StateMachineBehaviour
         FirstPersonController.disable = true;
         if (time > jumpScareDuration)
         {
+            camera.transform.localPosition = new Vector3(0, 0, 0);
+            arms.transform.localPosition = new Vector3(
+                arms.transform.localPosition.x,
+                arms.transform.localPosition.y,
+                0
+            );
             player.GetComponent<Rigidbody>().useGravity = true;
             FirstPersonController.disable = false;
             SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
@@ -49,6 +64,14 @@ public class JumpScareBehavior : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        arms = GameObject.FindGameObjectWithTag("Arms");
+        camera.transform.localPosition = new Vector3(0, 0, 0);
+        arms.transform.localPosition = new Vector3(
+            arms.transform.localPosition.x,
+            arms.transform.localPosition.y,
+            0
+        );
         player.GetComponent<Rigidbody>().useGravity = true;
         FirstPersonController.disable = false;
     }
