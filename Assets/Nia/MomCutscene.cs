005e6f3b02b61c2e28768ed959a7a6a36381be62
MauriceELkouby <151778578+MauriceELkouby@users.noreply.github.com>
2026-02-01 10:56:30 -0500

    intro and stuff prob

--- Assets/Nia/MomCutscene.cs
+++ Assets/Nia/MomCutscene.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void NextScene()
     {
-        //  SceneManager.LoadScene(__REPLACE__);
+        SceneManager.LoadScene("Mohammed_scene");
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
